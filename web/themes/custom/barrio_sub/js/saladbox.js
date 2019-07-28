c6316ce2a783e21e5114fbeb37c503dd1908de15
Unknown <webdev@permaculture.org.uk>
2019-07-28 08:49:12 +0100

    jquery to hide deprecated radio options - success

--- web/themes/custom/barrio_sub/js/saladbox.js
+++ web/themes/custom/barrio_sub/js/saladbox.js
@@ -3,7 +3,13 @@
     
     $(document).ready(function () {
 
-        $('#edit-field-drop-off-preference option[value="Marsden"]').addClass("highlight");
+        // hide Marsden option in "Your pick up preference" for Salad Box
+        $('#edit-field-drop-off-preference-marsden').hide();
+        $("label[for=edit-field-drop-off-preference-marsden],#edit-field-drop-off-preference-marsden").hide();
+
+        // hide Slaithwaite option in "Your pick up preference" for Salad Box
+        $('#edit-field-drop-off-preference-slaithwaite').hide();
+        $("label[for=edit-field-drop-off-preference-slaithwaite],#edit-field-drop-off-preference-slaithwaite").hide();
 
     });
 
