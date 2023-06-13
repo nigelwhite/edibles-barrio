adef0c3fa843ac2e6b9da2af4f374cdf65e83c6c
nigelwhite <webdev@permaculture.org.uk>
2023-06-13 11:42:42 +0100

    hide is fully working

--- web/themes/custom/barrio_sub/js/hidePayment.js
+++ web/themes/custom/barrio_sub/js/hidePayment.js
@@ -1,14 +1,11 @@
+// hides Edibles Stripe payment on Product if a hide box is ticked
+
 (function($, window, Drupal) {
   Drupal.behaviors.barrio_subhidePayment = {
     attach: function(context, settings) {
       if ($("div.product-hide-payment div div.field__item").text() == "1") {
         $(".col-sm-4").hide();
       }
-
-      $(".field--type-text-with-summary p").css("background-color", "yellow");
-      // $("div.product-hide-payment div div.field__item").insertAfter(
-      //   ".field--type-text-with-summary p"
-      // );
     }
   };
 })(jQuery, window, Drupal);
