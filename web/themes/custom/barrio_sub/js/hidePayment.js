a5ba78e8882e7059715af0e95c2a1724ad2ac179
nigelwhite <webdev@permaculture.org.uk>
2023-06-13 07:42:38 +0100

    glimmer

--- web/themes/custom/barrio_sub/js/hidePayment.js
+++ web/themes/custom/barrio_sub/js/hidePayment.js
@@ -1,10 +1,10 @@
 (function($, window, Drupal) {
   Drupal.behaviors.hidePayment = {
     attach: function(context, settings) {
-      var hideIt = $("div.product-hide-payment div div.field__item").text();
-      // if (hideIt == "1") {
+      if ($("div.product-hide-payment div div.field__item").text() == "1") {
+        $(".col-sm-4").show();
+      }
 
-      $(".col-sm-4").show();
       $(".field--type-text-with-summary p").css("background-color", "yellow");
       $("div.product-hide-payment div div.field__item").insertAfter(
         ".field--type-text-with-summary p"
