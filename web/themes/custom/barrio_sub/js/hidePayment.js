6e3282e8c5602976aa4adea1038a00031e046429
nigelwhite <webdev@permaculture.org.uk>
2023-06-13 07:37:17 +0100

    still not broken

--- web/themes/custom/barrio_sub/js/hidePayment.js
+++ web/themes/custom/barrio_sub/js/hidePayment.js
@@ -1,10 +1,14 @@
 (function($, window, Drupal) {
   Drupal.behaviors.hidePayment = {
     attach: function(context, settings) {
-      var hideIt = $("div.product-hide-payment div div.field__item").val();
+      var hideIt = $("div.product-hide-payment div div.field__item").text();
       // if (hideIt == "1") {
 
       $(".col-sm-4").show();
+      $(".field--type-text-with-summary p").css("background-color", "yellow");
+      $("div.product-hide-payment div div.field__item").insertAfter(
+        ".field--type-text-with-summary p"
+      );
     }
   };
 })(jQuery, window, Drupal);
